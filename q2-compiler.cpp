b6cf61d4817cf65289c9efe94ffb441654202611
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-08-22 19:47:32 -0500

    added the q2 compiler

---
+++ q2-compiler.cpp
@@ -0,0 +1,12 @@
+// q2-compiler
+// Using cout and printf to output some messages
+// csc121
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!! \nMy name is Azizbek Shokosimov";
+    printf("\nThis is Q2 and I am using Visual Studio Code on Mac");
+    return 0;
+}
