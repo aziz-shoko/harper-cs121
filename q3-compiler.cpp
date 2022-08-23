b02d507addb51c5f64c5783db4715be9634b3d05
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-08-22 20:30:16 -0500

    added q3 code

---
+++ q3-compiler.cpp
@@ -0,0 +1,13 @@
+// q3-compiler
+// Using cout and printf to output some messages
+// 08/22/2022
+// csc121
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello User\n";
+    printf("Azizbek Shokosimov \n08/22/2022");
+    return 0;
+}
