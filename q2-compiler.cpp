1dea14ecde2bda96085c74b5ae74a161685d44b1
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-08-25 19:45:29 -0500

    added comments

--- q2-compiler.cpp
+++ q2-compiler.cpp
@@ -1,13 +1,18 @@
-// q2-compiler
-// Using cout and printf to output some messages
-// 08/22/2022
-// csc121
+/* 
+Azizbek Shokosimov
+q2-compiler
+Using cout and printf to output some messages
+08/22/2022
+csc121
+*/
 
-#include <iostream>
-using namespace std;
+#include <iostream>                                                   // Declares the cout << and printf command
+using namespace std;                                                  // Allows us to use the cout << command without std::
 
-int main() {
-    cout << "Hello World!! \nMy name is Azizbek Shokosimov";
-    printf("\nThis is Q2 and I am using Visual Studio Code on Mac");
-    return 0;
+int main() {                                                          // Start of program
+    cout << "Hello World!! \nMy name is Azizbek Shokosimov";          // Outsputs text in the quotes with cout <<
+    printf("\nThis is Q2 and I am using Visual Studio Code on Mac");  // Outsputs text in the quotes with prinf 
+    return 0;                                                         // Ends the program
 }
+
+// run g++ -o <compiler-name>  <filename>.cpp to compile the code
