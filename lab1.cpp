feacd9e4cf01cb65cdc9ac1c33b85cfd138b6e3a
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-08-27 17:28:12 -0500

    final for lab1

--- lab1.cpp
+++ lab1.cpp
@@ -7,38 +7,29 @@ csc121
 */
 
 //the following are UBUNTU/LINUX, and MacOS ONLY terminal color codes.
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
-#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define RESET       "\033[0m"              /* End Color */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
 #define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
 #define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
-#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
-#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
-#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
-#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main() {             
-    cout << BOLDMAGENTA <<"\n\tName: Azizbek Shokosimov";                                         // Output Name in new line in red and tabbed format
+    cout << BOLDMAGENTA <<"\n\tName: Azizbek Shokosimov";                                           // Output Name in new line in magenta and tabbed format
     cout << "\n\tDate: 08/27/2022";                                                                 // Output Date in new line with tab
     cout << "\n\tLab: Week 1 Lab";                                                                  // Output Lab Number in new line with tab
     cout << "\n\tTitle: Tic Tac Toe Project";                                                       // Output Title in new line with tab
-    cout << "\n\tObjective: Create a tic tac toe board compromised of 1 through 9 digits" << RESET;          // Output Objective in new line with tab
+    cout << "\n\tObjective: Create a tic tac toe board compromised of 1 through 9 digits" << RESET; // Output Objective in new line with tab
     
-    cout << BOLDRED << "\n\n\n\n\t1" << setw(10) << "2" << setw(10) << "3";
-    cout << "\n\n\n\n\t4" << setw(10) << "5" << setw(10) << "6";
-    cout << "\n\n\n\n\t7" << setw(10) << "8" << setw(12) << "9\n\n" << RESET << endl;
+    cout << BOLDGREEN <<"\n\n\n\n\t|---------|----------|---------|";
+    cout << "\n\t| Tic" << setw (10) << "| Tac" << setw(11) << "| Toe" << setw(6) << "|";
+    cout << "\n\t|---------|----------|---------|" << RESET;
+
+    cout << BOLDRED << "\n\n\n\n\t 1" << setw(10) << "2" << setw(10) << "3";
+    cout << "\n\n\n\n\t 4" << setw(10) << "5" << setw(10) << "6";
+    cout << "\n\n\n\n\t 7" << setw(10) << "8" << setw(12) << "9\n\n" << RESET << endl;
     
     return 0;
 }
