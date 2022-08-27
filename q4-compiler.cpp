b82a8fa1811dfbc84df4361fbe8d42c6dd24a4e8
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-08-27 11:06:51 -0500

    added the color code for mac/linux

--- q4-compiler.cpp
+++ q4-compiler.cpp
@@ -6,12 +6,31 @@ Using setw and escape seq to manipulate WYSIWYG
 csc121
 */
 
+//the following are UBUNTU/LINUX, and MacOS ONLY terminal color codes.
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
 #include <iostream>                        // Global Library                             
 #include <iomanip>                         // Library for setw
 using namespace std;                       // Setting namespace to avoid std::
 
-int main() {                                                                                        // Starting Function 
-    cout << "\n\tName: Azizbek Shokosimov";                                                         // Output Name in new line and tabbed format
+int main() {             
+    cout << RED << "\n\tName: Azizbek Shokosimov";                                                         // Output Name in new line and tabbed format
     cout << "\n\tDate: 08/26/2022";                                                                 // Output Date in new line with tab
     cout << "\n\tLab: Lab 1";                                                                       // Output Lab Number in new line with tab
     cout << "\n\tTitle: Discussion Board Q4";                                                       // Output Title in new line with tab
