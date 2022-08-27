4fa955a45853fa2dbd1d8cdd3df40f51815a12cb
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-08-27 17:31:53 -0500

    comment

--- lab1.cpp
+++ lab1.cpp
@@ -12,24 +12,24 @@ csc121
 #define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
 #define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
 
-#include <iostream>
-#include <iomanip>
-using namespace std;
+#include <iostream>            // cout library
+#include <iomanip>             // setw library
+using namespace std;           // sets namespace to std to avoid std::
 
-int main() {             
+int main() {                                                                                        // Start Program
     cout << BOLDMAGENTA <<"\n\tName: Azizbek Shokosimov";                                           // Output Name in new line in magenta and tabbed format
     cout << "\n\tDate: 08/27/2022";                                                                 // Output Date in new line with tab
     cout << "\n\tLab: Week 1 Lab";                                                                  // Output Lab Number in new line with tab
     cout << "\n\tTitle: Tic Tac Toe Project";                                                       // Output Title in new line with tab
     cout << "\n\tObjective: Create a tic tac toe board compromised of 1 through 9 digits" << RESET; // Output Objective in new line with tab
     
-    cout << BOLDGREEN <<"\n\n\n\n\t|---------|----------|---------|";
-    cout << "\n\t| Tic" << setw (10) << "| Tac" << setw(11) << "| Toe" << setw(6) << "|";
-    cout << "\n\t|---------|----------|---------|" << RESET;
+    cout << BOLDGREEN <<"\n\n\n\n\t|---------|----------|---------|";                               // Chooses the green color for title and outputs a line 
+    cout << "\n\t| Tic" << setw (10) << "| Tac" << setw(11) << "| Toe" << setw(6) << "|";           // Outputs Tic Tac Toe title 
+    cout << "\n\t|---------|----------|---------|" << RESET;                                        // Outputs the last line
 
-    cout << BOLDRED << "\n\n\n\n\t 1" << setw(10) << "2" << setw(10) << "3";
-    cout << "\n\n\n\n\t 4" << setw(10) << "5" << setw(10) << "6";
-    cout << "\n\n\n\n\t 7" << setw(10) << "8" << setw(12) << "9\n\n" << RESET << endl;
+    cout << BOLDRED << "\n\n\n\n\t 1" << setw(10) << "2" << setw(10) << "3";                        // Outputs evenly spaced 1, 2, 3
+    cout << "\n\n\n\n\t 4" << setw(10) << "5" << setw(10) << "6";                                   // Outputs evenly spaced 4, 5, 6
+    cout << "\n\n\n\n\t 7" << setw(10) << "8" << setw(12) << "9\n\n" << RESET << endl;              // Outputs evenly spaced 7, 8, 9
     
-    return 0;
+    return 0;                                                                                       // End program
 }
