bc2045ff06e41b5464a851650ecda8645d07c048
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-08-30 09:08:53 -0500

    update q1 compiler

--- Week-2/q1-compiler.cpp
+++ Week-2/q1-compiler.cpp
@@ -11,18 +11,21 @@ csc121
 using namespace std;             // Used to avoid std::
 
 string name;                     // Declared variable name but not initialized
-int age;                         // Declared variable age but not initialized
-int gpa;                         // Declared variable gpa but not initialized
+int age{};                         // Declared variable age but not initialized
+int gpa{};                         // Declared variable gpa but not initialized
 
-int main() {                                                                                // Start Function
-    cout << "What is you name? ";                                                           // Output What is your name?
-    cin >> name;                                                                            // Store user input in name variable
-    cout << "what is your age? ";                                                           // Output What is your age?
-    cin >> age;                                                                             // Store user input in age variable
-    cout << "What is your gpa? ";                                                           // Output What is your gpa?
-    cin >> gpa;                                                                             // Store user input in gpa
-    cout << name << "'s new age is " << age + 4 << " and new gpa is " << gpa + 1.3 << endl; // Output name, new age, and new gpa
-    return 0;                                                                               // End function
+int main() {                                                                                    // Start Function
+    cout << "What is you name? ";                                                               // Output What is your name?
+    cin >> name;                                                                                // Store user input in name variable
+
+    cout << "what is your age? ";                                                               // Output What is your age?
+    cin >> age;                                                                                 // Store user input in age variable
+
+    cout << "What is your gpa? ";                                                               // Output What is your gpa?
+    cin >> gpa;                                                                                 // Store user input in gpa
+
+    cout << name << "'s new age is " << (age + 4) << " and new gpa is " << (gpa + 1.3) << endl; // Output name, new age, and new gpa
+    return 0;                                                                                   // End function
 }
 
 /*
