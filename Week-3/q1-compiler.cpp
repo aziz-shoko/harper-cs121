d9ac6e36030747bf3e1ab84dd8ef8b4394eab09e
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-09-06 09:29:46 -0500

    finished q1

--- Week-3/q1-compiler.cpp
+++ Week-3/q1-compiler.cpp
@@ -10,6 +10,10 @@ csc121
 using namespace std;
 
 void posNeg(double);
+void evenOdd(int);
+void divThree(int);
+void multiFive(int);
+void zeroHundred(double);
 
 int main() {
     double numb{};
@@ -18,6 +22,10 @@ int main() {
     cin >> numb;
     
     posNeg(numb);
+    evenOdd(numb);
+    divThree(numb);
+    multiFive(numb);
+    zeroHundred(numb);
 
 }
 
@@ -28,5 +36,37 @@ void posNeg(double numb) {
         cout << numb << " = Negative Number\n";
     } else if ( numb == 0) {
         cout << numb << " = Zero\n";
+    }
+}
+
+void evenOdd(int numb) {
+    if ( numb % 2 == 0 ) {
+        cout << numb << " = Even\n";
+    } else if ( numb % 2 != 0 ) {
+        cout << numb << " = Odd\n";
+    }
+}
+
+void divThree(int numb) {
+    if ( numb % 3 == 0 ) {
+        cout << numb << " = Divisable by Three\n";
+    } else if ( numb % 3 != 0 ) {
+        cout << numb << " = Not Divisable by Three\n";
+    }
+}
+
+void multiFive (int numb) {
+    if ( numb % 5 == 0 ) {
+        cout << numb << " = Multiple of Five\n";
+    } else if ( numb % 5 != 0 ) {
+        cout << numb << " = Not multiple of 5\n";
+    }
+}
+
+void zeroHundred (double numb) {
+    if ( numb >= 0 && numb <= 100 ) {
+        cout << numb << " = Between 0 to 100\n";
+    } else {
+        cout << numb << " = Not Between 0 to 100\n";
     }
 }
