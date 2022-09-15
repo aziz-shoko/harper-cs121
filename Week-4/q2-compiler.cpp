4f31693b02e3b3a157130c4fb8af378132ac3bb0
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-09-14 22:57:50 -0500

    fixed q2

--- Week-4/q2-compiler.cpp
+++ Week-4/q2-compiler.cpp
@@ -6,41 +6,41 @@ Use Switch and Case to determine grade average
 csc121
 */
 
-#include <iostream> 
-using namespace std;
+#include <iostream>                                 // File header for cin and cout       
+using namespace std;                                // avoid suing std
 
-int num{1}, num1{2}, num2{3}, average{}, final{};
+int num{1}, num1{2}, num2{3}, average{};            // declare variables num num1 num2 average
 
 
-void caseSwitch(int);
-void validator(int);
+void caseSwitch(int);                               // declare caseSwich function
+void validator(int);                                // declare validator function 
 
 int main() {
-    cout << "Please enter your first score: ";
-    cin >> num;
+    cout << "Please enter your first score: ";      // Output text in quotes
+    cin >> num;                                     // store user input in num
 
-    validator(num);
-    caseSwitch(num);
+    validator(num);                                 // call function validator and pass variable num
+    caseSwitch(num);                                // call function caseSwitch and pass variable num
 
-    cout << "Please enter your second score: ";
-    cin >> num1;
+    cout << "Please enter your second score: ";     // Output text in quotes
+    cin >> num1;                                    // store user input in num1
 
-    validator(num1);
-    caseSwitch(num1);
+    validator(num1);                                // call function validator and pass variable num1
+    caseSwitch(num1);                               // call function caseSwitch and pass variable num1
 
-    cout << "Please enter your third score: "; 
-    cin >> num2;
+    cout << "Please enter your third score: ";      // Output text in quotes
+    cin >> num2;                                    // store user input in num2
 
-    validator(num2);
-    caseSwitch(num2);
+    validator(num2);                                // call function validator and pass variable num2                
+    caseSwitch(num2);                               // call function caseSwitch and pass variable num2
     
-    int average{(num + num1 + num2)/3};
+    int average{(num + num1 + num2)/3};             // declare variable average and set value to average of 3 nums                    
     
-    cout << "Your final grade is ";
-    caseSwitch(average);
+    cout << "Your final grade is ";                 // Output text in quotes
+    caseSwitch(average);                            // call function caseSwitch  
 }
 
-void validator(int num, int num1, int num2) {              
+void validator(int num) {              
     if ( num < 0 ) {                                              // Check to see if num is less than 100
         cout << "\t\nPlease enter a valid num next time!\n\n";    // Output text in quotes
         exit(0);                                                  // Exit 
@@ -50,25 +50,25 @@ void validator(int num, int num1, int num2) {
     }
 }
 
-void caseSwitch(int num, int num1, int num2, int average){
-    switch (num/10 ) {
-        case (10):
-            cout << "Congratulations, A+\n";
-            break;
-        case (9): 
-            cout << "A\n";
-            break;
-        case (8):
-            cout << "B\n";
-            break;
-        case (7):
-            cout << "C\n";
-            break;
-        case (6):
-            cout << "D\n";
-            break;
-        default:
-            cout << "F\n";
+void caseSwitch(int num){
+    switch (num/10 ) {                                  // declare switch condition (take num/10)
+        case (10):                                      // case for perfect score
+            cout << "Congratulations, A+\n";            // Output in text in quotes
+            break;                                      // Exit switch/case function
+        case (9):                                       // Case for letter grade A
+            cout << "A\n";                              // Output in text in quotes
+            break;                                      // Exit switch function
+        case (8):                                       // Case for letter grade B
+            cout << "B\n";                              // Output text in quotes
+            break;                                      // exit switch funciton
+        case (7):                                       // case for letter grade C
+            cout << "C\n";                              // Output in text in quotes
+            break;                                      // Exit switch function
+        case (6):                                       // case for letter grade D
+            cout << "D\n";                              // Output in text in quotes
+            break;                                      // Exit function
+        default:                                        // if all cases fail, then default to letter F
+            cout << "F\n";                              // Output in text in quotes
     }
 }
 
