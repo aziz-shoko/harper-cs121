a6267789f6aed155f1fcc637ff863afd92133693
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-09-19 21:08:04 -0500

    down prime number

--- Week-5/q1-compiler.cpp
+++ Week-5/q1-compiler.cpp
@@ -30,10 +30,14 @@ int main() {
 
 void down(string) {
     for ( i = 1; i <= 100; i++) {
-        if ( i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0 && i%9 !=0) {
+        if ( i >= 10) {
+            if ( i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0 && i%9 !=0) {
             continue;
+            }
+            cout << i << endl;
+        } else if ( i <= 10 && i != 1 && i != 2 && i != 3 && i != 5 && i != 7) {
+            cout << i << endl;
         }
-        cout << i << endl;
     }
 }
 
