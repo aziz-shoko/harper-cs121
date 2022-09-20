063a89198ae25dacb64711cfbc9a60ba5bb65c3f
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-09-19 21:37:49 -0500

    comment

--- Week-5/q1-compiler.cpp
+++ Week-5/q1-compiler.cpp
@@ -6,54 +6,54 @@ We will use counters
 csc121
 */
 
-#include <iostream> 
-#include <string>
-using namespace std;
+#include <iostream>                         // cin and cout file header
+#include <string>                           // string input file header
+using namespace std;                        // avoid using std::
 
-void up(string);
-void down(string);
+void up(string);                            // Declare function up
+void down(string);                          // Declare funciton down
 
-int i{};
-string userInput{};
+int i{};                                    // Declare int variable i
+string userInput{};                         // Declare string varaible userInput
 
 int main() {
-    cout << "Enter up or down: ";
-    cin >> userInput;
+    cout << "Enter up or down: ";           // Output text in quotes
+    cin >> userInput;                       // Store user input in userInput
 
-    if ( userInput == "up") {
-        up(userInput);
-    } else if ( userInput == "down" ) {
-        down(userInput);
+    if ( userInput == "up") {               // condition for if user chooses up
+        up(userInput);                      // calls function up and passes userInput into the function
+    } else if ( userInput == "down" ) {     // condition for if user chooses down
+        down(userInput);                    // call function down and passes userInput into the function
     }
 
 }
 
-void down(string) {
-    for ( i = 1; i <= 100; i++) {
-        if ( i >= 10) {
-            if ( i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0 && i%9 !=0) {
-            continue;
+void down(string) {                                                                 //  down function
+    for ( i = 1; i <= 100; i++) {                                                   // for loop for outputting numbers between 1 and 100 (up)
+        if ( i >= 10) {                                                             // condition for cheacking if i is greater than 10
+            if ( i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0 && i%9 !=0) {         // if i isn't even, divisible by 3, 5, 7, 9 then its prime
+            continue;                                                               // skips the prime numbers
             }
-            cout << i << endl;
-        } else if ( i <= 10 && i != 1 && i != 2 && i != 3 && i != 5 && i != 7) {
-            cout << i << endl;
+            cout << i << endl;                                                      // outputs 10 to 100 without prime numbers
+        } else if ( i <= 10 && i != 1 && i != 2 && i != 3 && i != 5 && i != 7) {    // condition for checking if i is less than 10 and not = to 1,2,3,5,7
+            cout << i << endl;                                                      // output i that is not prime for numbers 1 to 10
         }
     }
 }
 
-void up(string) {
-    for ( i = 100; i >= 1; i-- ) {
-        if ( i >= 10) {
-            if ( i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0 && i%9 !=0) {
-                cout << "\n" << i;
-            } else {
-                cout << " " << i;
+void up(string) {                                                                   // up function
+    for ( i = 100; i >= 1; i-- ) {                                                  // for loop for outputting numbers between 1 to 100 (down)
+        if ( i >= 10) {                                                             // condition for i greater than or equal to 10
+            if ( i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0 && i%9 !=0) {         // condition for prime numbers
+                cout << "\n" << i;                                                  // out prime numbers in new line
+            } else {                                                                    
+                cout << " " << i;                                                   // output every other number besides prime
             }
-        } else if ( i < 10 ) {
-            if ( i == 1 || i ==2 || i == 3 || i == 5 || i == 7){
-                cout << "\n" << i;
+        } else if ( i < 10 ) {                                                      // condition for i less than 10
+            if ( i == 1 || i ==2 || i == 3 || i == 5 || i == 7){                    // condition for i equaling either 1, 2, 3, 5, 7
+                cout << "\n" << i;                                                  // output prime in new line
             } else {
-                cout << " " << i;
+                cout << " " << i;                                                   // output other numbers besides prime
             }
         }
     }
