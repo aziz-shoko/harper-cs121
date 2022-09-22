ad92930c3259f596d65fcf18dd8cb33e2c763a23
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-09-22 16:08:37 -0500

    comments q2 shorter

--- Week-5/q2-shorter.cpp
+++ Week-5/q2-shorter.cpp
@@ -16,27 +16,27 @@ int i{0};                                                               // decla
 double score{0}, total{0}, avg{0}; // Declare the needed variables
 string valid{};
 
-int main() {                                                            // maint function start
-    for (i=1; i<=5; i++) {
-        cout << "Enter score " << i << ": ";
-        cin >> score;
-        valid = ( score < 0 || score > 100 || cin.fail()) ? "Invalid" : "Valid";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        validator(valid);
-        if ( valid == "Valid") {
-            total += score;
+int main() {                                                                        // maint function start
+    for (i=1; i<=5; i++) {                                                          // Runs code 5 times   
+        cout << "Enter score " << i << ": ";                                        // Output Enter score value of i
+        cin >> score;                                                               // Store user input
+        valid = ( score < 0 || score > 100 || cin.fail()) ? "Invalid" : "Valid";    // condition for checking the validity of score
+        cin.clear();                                                                // clear error state from bad input
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');                        // prevent infinite loop
+        validator(valid);                                                           // call validator function
+        if ( valid == "Valid") {                                                    // Get total only if valid is Valid
+            total += score;                                                         // calculate total
         }
     }
-    avg = total/5;
-    cout << "Your average is score: " << avg << endl;
-    cout << "Your letter grade is " << getLetterGrade(avg) << endl;
+    avg = total/5;                                                                  // Calculate average
+    cout << "Your average is score: " << avg << endl;                               // Output in quotes
+    cout << "Your letter grade is " << getLetterGrade(avg) << endl;                 // Output in quotes and call function getLetterGrade
 }
 
 void validator(string) {              
-    if ( valid == "Invalid") {                                     // Check to see if score is between 0 and 100
+    if ( valid == "Invalid") {                                      // Check to see if score is between 0 and 100
         cout <<"\nScore outta range (1-100!)\n\n";                  // Output text in quotes
-        i--;
+        i--;                                                        // force loop to rerun
     }
 }
 string getLetterGrade(double){
