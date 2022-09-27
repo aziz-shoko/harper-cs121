9899613597ae58c6a33159a9a3c70ac9e0d0ed14
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-09-27 15:53:13 -0500

    q5 comment

--- Week-5/q5-compiler.cpp
+++ Week-5/q5-compiler.cpp
@@ -6,27 +6,27 @@ Spell name backwords and output vowels in name
 csc122
 */
 
-#include <iostream>
-using namespace std;
+#include <iostream>                             // cout and cin file header
+using namespace std;                            // avoid using std::
 
-string name{};
+string name{};                                  // declare string var name
 
 int main() {
-    cout << "Enter you name: ";
-    cin >> name;
-    transform(name.begin(), name.end(), name.begin(), ::tolower);
+    cout << "Enter you name: ";                                                                         // output text in quotes
+    cin >> name;                                                                                        // store user input in name
+    transform(name.begin(), name.end(), name.begin(), ::tolower);                                       // make user input to lowercase
 
-    cout << "Vowels: ";
-    for (int i = 0; i < name.length(); i++ ){
-        if (name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i] == 'u'){
-            cout << name[i] << " ";
+    cout << "Vowels: ";                                                                                 // output text in quotes
+    for (int i = 0; i < name.length(); i++ ){                                                           // loops around based on the length of name
+        if (name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i] == 'u'){    // condition for vowels
+            cout << name[i] << " ";                                                                     // output the vowels
         }
     }
     
-    cout << "\nName backwards: ";
-    for (int i = name.length()-1; i >= 0; i-- ){
-        cout << name[i];
-    }
-    cout << "\n";
+    cout << "\nName backwards: ";                                                                       // output text in quotes
+    for (int i = name.length()-1; i >= 0; i-- ){                                                        // loop through the name backwords
+        cout << name[i];                                                                                // output the name in backwords
+    }   
+    cout << "\n";                                                                                       // newline because linux bad and output a percent sign for no reason
 }
 
