f3eeacdc441423ab6425abf6b08b36012ac94e89
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-09-30 17:36:25 -0500

    added comments

--- Week-5/Lab5.cpp
+++ Week-5/Lab5.cpp
@@ -20,22 +20,18 @@ using namespace std;           // avoid using std::
 #define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
 
 void title(void);                                               // Declare function title
-void game(void);
+void game(void);                                                // Declare function game
 void display(char);                                             // Declay function display
 
 char num[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};   // Declare num and create array 1 - 9
-char userNum{};                                                 // Declare userNum variable
+char userNum{}, beta{}, x{'a'}, y{'b'};                         // Declare userNum, beta, x, b variables
 int alpha{1};                                                   // Declare variable alpha
-char beta{};                                                    // Declare variable beta
 string userInput{};                                             // Declare variable userInput
-char x{'a'}, y{'a'}; 
-
+                                          
 int main() {
-    char null{};                                            // create emtpy char variable
-
     title();                                                // call function title
-    display(null);                                          // call function display and pass on var null (silences too few arguments error)
-    game();
+    display(userNum);                                       // call function display and pass on var null (silences too few arguments error)
+    game();                                                 // call function game
 }
 
 void title(){
@@ -43,7 +39,7 @@ void title(){
     cout << "\n\tDate: 09/29/2022";                                                                 // Output Date in new line with tab
     cout << "\n\tLab: Week 5 Lab";                                                                  // Output Lab Number in new line with tab
     cout << "\n\tTitle: Tic Tac Toe Project";                                                       // Output Title in new line with tab
-    cout << "\n\tObjective: Create a tic tac toe game" << RESET;                                   // Output Objective in new line with tab
+    cout << "\n\tObjective: Create a tic tac toe game" << RESET;                                    // Output Objective in new line with tab
 
     cout << BOLDGREEN <<"\n\n\n\n\t|---------|----------|---------|";                               // Chooses the green color for title and outputs a line 
     cout << "\n\t| Tic" << setw (10) << "| Tac" << setw(11) << "| Toe" << setw(6) << "|";   // Outputs Tic Tac Toe title 
@@ -51,17 +47,17 @@ void title(){
 }
 
 void game() {
-    for (int i = 1; i > 0; i++ ) {                                          // Infinite loop
+    for (int i = 1; i > 0; i++ ) {                                          // Infinite  for loop
         if ( alpha%2 == 1) {                                                // checks if alpha modules 2 is equal to 1
             beta = 'X';                                                     // Sets beta value to char X
-            cout << "Player " << beta <<" enter a number (1-9): ";                  // Ouput player x enter number
-            cin >> userInput; 
+            cout << "Player " << beta <<" enter a number (1-9): ";          // Ask player X to input a number
+            cin >> userInput;                                               // Store user input in userInput
         } else {
             beta = 'O';                                                     // Sets beta value to char O
-            cout << "The Bot is thinking..." << endl;
-            usleep(500000);                                                // sleeps for 1 second before increasing s by 1
-            srand(time(0));
-            userInput = to_string(rand() % 9 + 1);
+            cout << "The Bot is thinking..." << endl;                       // Output Bot is thinking
+            usleep(500000);                                                 // sleeps for 0.5 second before increasing s by 1
+            srand(time(0));                                                 // this forces the rand function to choose new num everytime
+            userInput = to_string(rand() % 9 + 1);                          // Choose a random number between 1 and 9
         } 
         
         if (userInput.length() == 1 ) {                                         // Condition to see if user inputed 1 char 
@@ -73,9 +69,9 @@ void game() {
         if ( userNum != '1' && userNum != '2' && userNum != '3' && userNum != '4' && userNum != '5' && userNum != '6' && userNum != '7' && userNum != '8' && userNum != '9' ) {
             cout << BOLDRED << "\nInvalid! Enter number 1 - 9: \n\n" << RESET;                          // Condition to see if user inputted number between 1 through 9
         } else {
-            x=y;
+            x=y;                                                                                        // Sets x equal to y to force 
             char X{ (userNum == num[0] && num[0] != 'O' && num[0] !='X') ?                              // Condition for player X choosing between 1 - 9
-                num[0] = beta, x='b':                                                                          // Once the player chooses the square, the value cannot be changed if its O or X
+                num[0] = beta, x='b':                                                                   // Once the player chooses the square, the value cannot be changed if its O or X
                     (userNum == num[1] && num[1] != 'O' && num[1] !='X') ? 
                         num[1] = beta, x='b': 
                             (userNum == num[2] && num[2] != 'O' && num[2] !='X') ? 
@@ -92,7 +88,7 @@ void game() {
                                                                         num[7] = beta, x='b': 
                                                                             (userNum == num[8] && num[8] != 'O' && num[8] !='X') ? 
                                                                                 num[8] = beta, x='b': '!'}; 
-            if (x==y) {
+            if (x==y) {                                                    // This part is for validating and preventing the override of already set chars
                 alpha--;                                                   // Decreases values of alpha by 1 so the program is forced to loop back to the current players turn 
                 cout << BOLDRED << "\nCannot Override! Try again: ";       // and ask the player to Try again Try again: 
             }
@@ -132,7 +128,7 @@ void display(char) {
         cout << BOLDGREEN << "Congratulations! Player " << beta << " wins!\n\n";;
         exit(0);
     } else if (num[0] != '1' && num[1] != '2' && num[2] != '3' && num[3] != '4' && num[4] != '5' && num[5] != '6' && num[6] != '7' && num[7] != '8' && num[8] != '9') {
-        cout << BOLDYELLOW << "Its a Tie!" << endl;
+        cout << BOLDYELLOW << "Its a Tie!" << endl;                                 // Above is the condition in case of tie
         exit(0);
     }
 }
