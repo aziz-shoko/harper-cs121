095b90586b14e7ef5b194046efa96c18e6d84222
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-10-03 22:26:45 -0500

    nick champ

--- Week-7/q1-compiler.cpp
+++ Week-7/q1-compiler.cpp
@@ -7,4 +7,43 @@ csc121
 */
 
 #include <iostream>
+#include <vector>
 using namespace std; 
+
+int protectData(string);
+
+int total{}, userNum{-1};
+vector<int> nums; 
+string input{"y"};
+
+int main() {
+    while ( input == "y") {
+        total += protectData(input);
+    }
+    cout << total << endl;
+
+    for (int j = 0; j < nums.size(); j++) {
+        cout << nums[j] << endl;
+
+    }
+}
+
+int protectData(string) {
+    userNum = -1;
+    while ( userNum < 0 || userNum > 100) {
+        cout << "Enter your score: ";
+        cin >> userNum;
+    } 
+    nums.push_back(userNum);
+    input = "k";
+    while ( input != "n" && input != "y" ) {
+        cout << "Would like to repeat? (y/n): ";
+        cin >> input;
+    }
+    
+    return userNum;
+}
+
+
+
+
