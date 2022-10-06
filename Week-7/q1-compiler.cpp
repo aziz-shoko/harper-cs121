e905bd9c8ab06443b8c68fd95a293ae1a086782c
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-10-06 17:23:14 -0500

    comments

--- Week-7/q1-compiler.cpp
+++ Week-7/q1-compiler.cpp
@@ -10,39 +10,39 @@ csc121
 using namespace std;                                                    // avoid using std
 
 int protectData(string);                                                // protect data function
-int total{}, userNum{}, userNumbers[10]{}, i{0};                        // declare int vars
+int total{}, userNum{}, userNumbers[10]{0}, i{0}, getArrayLength{};     // declare int vars
 string input="y";                                                       // decalre input string
 
 int main() {
     while ( input == "y") {                                             // condition for while loop
-        total += protectData(input);                                    // total eqaution
+        total += protectData(input);                                    // total equation
     }
 
-    int getArrayLength = sizeof(userNumbers) / sizeof(int);             // gets the array length
+    getArrayLength = sizeof(userNumbers) / sizeof(int);                 // gets the array length
     for (int j = 0; j < getArrayLength; j++) {                          // for loop for printing out the userNumbers
         cout << "User #: "<< j << " Score: "<< userNumbers[j] << endl;  // Outputs users 
     }
-    cout << "\nTotal Score: " << total << endl;
+    cout << "\nTotal Score: " << total << endl;                         // output the total score
 }
 
-int protectData(string) {
-    cout << "Enter your score: ";
-    cin >> userNum;
-    while (userNum < 0 || userNum > 100 || cin.fail()) {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Enter valid number! (1 - 100): ";
-            cin >> userNum;
+int protectData(string) {                       
+    cout << "Enter your score: ";                                       // Output text in quotes
+    cin >> userNum;                                                     // store user input in userNum
+    while (userNum < 0 || userNum > 100 || cin.fail()) {                // Conditions for protecting data
+        cin.clear();                                                    // resets the buffer and clears the error state
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');            // prevent infite loop and ignore multiple char inputs
+        cout << "Enter valid number! (1 - 100): ";                      // output text in quotes
+        cin >> userNum;                                                 // store user input in userNum
     }
-    userNumbers[i] = userNum;
-    i++;
+    userNumbers[i] = userNum;                                           // set first element in userNumbers to userNum
+    i++;                                                                // increment i by +1
 
 
 
-    input = "k";
-    while ( input != "n" && input != "y" ) {
-        cout << "Would like to repeat? (y/n): ";
-        cin >> input;
+    input = "k";                                                        // simply changes value for input to k (bc default is y)
+    while ( input != "n" && input != "y" ) {                            // if input doesn't equal n or y, keep asking user if they want to repeat
+        cout << "Would like to repeat? (y/n): ";                        // output text in quotes
+        cin >> input;                                                   // store user input in input
     }
-    return userNum;
+    return userNum;                                                     // return userNum value
 }
