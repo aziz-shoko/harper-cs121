ec9f6548914beacad05700f4719c7e135c756dfe
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-10-09 11:26:23 -0500

    added q5 file

---
+++ Week-7/q5-compiler.cpp
@@ -0,0 +1,19 @@
+/*
+Azizbek Shokosimov
+Week7 Q5
+Create 4 while loops for random number
+10/9/2022
+csc121
+*/
+
+#include <iostream> 
+#include <random> 
+#include<ctime>
+using namespace std;
+ 
+int main()
+{
+ default_random_engine engine{ static_cast<unsigned int>(time(0)) };
+ uniform_int_distribution<unsigned int> randNum{ 1, 1000 };
+ cout << "\n\t" << randNum(engine) << endl;
+}
