b46168d24a13b623985787bd869ce170900c2b5c
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-10-10 09:29:09 -0500

    comment

--- Week-7/q5-compiler.cpp
+++ Week-7/q5-compiler.cpp
@@ -6,29 +6,29 @@ Create 4 while loops for random number
 csc121
 */
 
-#include <iostream> 
+#include <iostream>                                                     // file headers
 #include <random> 
 #include<ctime>
 using namespace std;
 
-int i{0}, num{};
+int i{0}, num{};                                                        // declare vars
 
 int main() {
     default_random_engine engine{ static_cast<unsigned int>(time(0)) };
     uniform_int_distribution<unsigned int> randNum{ 1, 1000 };
     cout << "\n\t" << randNum(engine) << endl;
 
-    cout << "Random 1 to 100: ";
+    cout << "Random 1 to 100: ";                                        // loop 1 for 1 to 100
     while ( i < 20 ) {
         num = randNum(engine);
         if (num <= 100) {
-            cout << num << " ";
+            cout << num << " ";                                         // output constrained number
             i++;
         }
     }
 
-    i = 0;
-    cout << "\nRandom 1 to 12: ";
+    i = 0;                                                              // reset i to 0
+    cout << "\nRandom 1 to 12: ";                                       // loop 2 for 1 to 12
     while ( i < 20 ) {
         num = randNum(engine);
         if (num <= 12) {
@@ -38,7 +38,7 @@ int main() {
     }
 
     i = 0;
-    cout << "\nRandom 1 to 6: ";
+    cout << "\nRandom 1 to 6: ";                                        // loop 3 for 1 to 6
     while ( i < 20 ) {
         num = randNum(engine);
         if (num <= 6) {
@@ -48,7 +48,7 @@ int main() {
     }
 
     i = 0;
-    cout << "\nRandom 1 to 4: ";
+    cout << "\nRandom 1 to 4: ";                                        // loop 4 for 1 to 4
     while ( i < 20 ) {
         num = randNum(engine);
         if (num <= 4) {
