3efee3dd7023ad76f09d5d8a340870d630a80be0
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-10-15 17:00:36 -0500

    q2 update

--- Week-8/q2-compiler.cpp
+++ Week-8/q2-compiler.cpp
@@ -1,17 +1,24 @@
-#include <iostream>
+#include <iostream> 
+#include <vector>															//need vector header to use vectors
 using namespace std;
 
+
 int main() {
-    try {
-        int age = 15;
-    if (age >= 18) {
-        cout << "Access granted - you are old enough.";
-    } else {
-        throw (age);
-    }
-    }
-    catch (int myNum) {
-        cout << "Access denied - You must be at least 18 years old.\n";
-        cout << "Age is: " << myNum;
-    }
+	string word;
+	bool repeat = true;
+	while (repeat) {
+		repeat = false;											
+		cout << "Please enter your word : ";
+		cin >> word;
+		cin.ignore(numeric_limits<streamsize> :: max(), '\n');								//ignores cin value
+		try {
+			cout << "\nThe 6th letter of your word is : '"									//it will try to evaluate word.at(5). if it works,
+				<< word.at(5) << "'.\n";													//the statement will be printed out
+		}
+		catch (out_of_range) {
+			repeat = true;																	//if try does not work and we catch an outofrange error
+			cout << "\nYour word isn't long enough for our purposes.\n"						//this statement is printed
+				<< "Please try again with a longer word...\n";
+		}
+	}
 }
