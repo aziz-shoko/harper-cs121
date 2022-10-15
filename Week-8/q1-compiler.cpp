5dd15d9ace23cf586aa0b098f2b02e7eaf828374
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-10-15 11:51:26 -0500

    q1 comments

--- Week-8/q1-compiler.cpp
+++ Week-8/q1-compiler.cpp
@@ -12,29 +12,29 @@ using namespace std;                                // avoid using std::
 
 string userInput{};                                 // string variable
 
-vector<string> names;
+vector<string> names;                               // declare string vector names
 
 int main() {
-    while (true) {
-        cout << "Enter a name: ";
-        cin >> userInput;
-        if (!cin.eof()) {
-            names.push_back(userInput);
+    while (true) {                                  // infinite loop
+        cout << "Enter a name: ";                   // output name in text
+        cin >> userInput;                           // store user input in the variable
+        if (!cin.eof()) {                           // condition for ctrl+Z
+            names.push_back(userInput);             // add users input into vector names
         } else {
-            break;
+            break;                                  // once ctrl+z, the break is applied to break out of loop
         }
     }
 
-    sort(names.begin(), names.end());
+    sort(names.begin(), names.end());   // sort function for vectors
 
-    cout << "\nNon Pop back Names: ";
+    cout << "\nNon Pop back Names: ";               // displays all names in vector
     for ( string items : names ){
         cout << items << " ";
     }
 
-    names.pop_back();
+    names.pop_back();                               // removes the last element in vector
     cout << "\nPop back Names: ";
-    for ( string items : names ){
+    for ( string items : names ){                   // display last element removed vector
         cout << items << " ";
     }
 
