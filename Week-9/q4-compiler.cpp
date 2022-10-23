ced6986e5bd3e2f87088c09fb2c6d266caba7139
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-10-22 19:08:11 -0500

    q4 comment

--- Week-9/q4-compiler.cpp
+++ Week-9/q4-compiler.cpp
@@ -6,21 +6,21 @@ Make factorial
 csc121
 */
 
-#include <iostream>
-using namespace std;
+#include <iostream>                                     // cin and cout file headers
+using namespace std;                                    // avoid using namespace std:: 
 
-int factorial(int);
+int factorial(int);                                     // declare function factorial
 
 int main() {
-    for (int i = 0; i < 11; i++) {
-        cout << i << "! = " << factorial(i) << endl;
+    for (int i = 0; i < 11; i++) {                      // run program 10 times
+        cout << i << "! = " << factorial(i) << endl;    // call function factorial and output the factorials
     }
 }
 
 int factorial(int i) {
-    if (i > 1) {
-        return i*factorial(i - 1);
+    if (i > 1) {                                        // as long as i is greater than 1,
+        return i*factorial(i - 1);                      // take i * call function again with i - 1 and return it
     } else {
-        return 1;
+        return 1;                                       // when i <= 1, return 1
     }
 }
