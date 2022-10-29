054cfa0aa79966c8c0509ea479b0af7c88095f82
aziz-shoko <azizbek.shokosimov@gmail.com>
2022-10-29 12:11:07 -0500

    q1

---
+++ Week-10/q1-compiler.cpp
@@ -0,0 +1,29 @@
+/*
+Azizbek Shokosimov
+Week10 Q1
+OFstream
+10/29/2022
+csc121
+*/
+
+/*
+The ofsteam needs fstream fileheader
+Ofstream is used to create and write into files
+The synxtax for opening a file: void open(const char *filename, ios::openmode mode);
+ofstream somename ("filename", argument);  this opens file
+file.close(); closes file and is optional for c++11
+ios::out -  overrides previous text
+
+ios::app - doesnt override previous text
+
+ios::in - reads text from the file.
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ofstream file ("names.txt");
+    file << "Azizbek Shokosimov";
+}
