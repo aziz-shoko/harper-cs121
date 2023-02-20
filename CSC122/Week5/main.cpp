fa6b072da84618ad0b0afea9bc76abfbe0cbab3e
aziz-shoko <azizbek.shokosimov@gmail.com>
2023-02-19 18:08:37 -0600

    added the adder function

--- CSC122/Week5/main.cpp
+++ CSC122/Week5/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 class bookList {
     private:
         string* library;
+        string* copyLibrary;
         size_t arrSize;
 
     public: 
@@ -14,8 +15,11 @@ class bookList {
         ~bookList();
         void Resize(size_t);
         void Setter(string*, size_t);
+        void Adder(string);
+        void Remover(string);
         string Getter();
         void Clear();
+
 };
 
 bookList::bookList() {}
@@ -29,6 +33,7 @@ bookList::~bookList() {
 void bookList::Resize(size_t size) {
     Clear();
     library = new string[size];
+    size_t num = sizeof(*library)/sizeof(library[0]);
 }
 
 void bookList::Setter(string* x, size_t size) {
@@ -49,9 +54,27 @@ string bookList::Getter() {
 
 void bookList::Clear() {
     delete[] library;
-    library = NULL;
-    arrSize = 0;
+    library = nullptr;
 }
+
+void bookList::Adder(string userBook) {
+    int newSize = arrSize + 1;
+    arrSize = arrSize + 1;
+    copyLibrary = new string[newSize];
+    for (int i = 0; i < newSize; i++) {
+        copyLibrary[i] = library[i];
+    }
+    copyLibrary[newSize-1] = userBook;
+    Resize(newSize);
+    for (int i = 0; i < newSize; i++) {
+        library[i] = copyLibrary[i];
+        cout << library[i] << endl;
+    }
+    delete[] copyLibrary;
+    copyLibrary = nullptr;
+}
+
+void bookList::Remover(string bookTitle) {}
 
 
 ostream& operator<< (ostream& out, bookList& y) {
@@ -60,8 +83,9 @@ ostream& operator<< (ostream& out, bookList& y) {
 }
 
 int main() {
-    int userInput;
+    int userInput = 0;
     string data[] = {"algebra", "geometry", "chemistry", "physics"};
+    string userBook;
     size_t size = sizeof(data)/sizeof(data[0]);
     bookList obj;
 
@@ -74,6 +98,11 @@ int main() {
 
         if (userInput == 1) {
             cout << obj;
+        } else if (userInput == 2) {
+            cout << "\nEnter a book name to add: ";
+            cin >> userBook;
+            obj.Adder(userBook);
+            
         }
     }
 
