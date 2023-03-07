feda203bb7ed2e2819800716d30d4856f39161dc
aziz-shoko <azizbek.shokosimov@gmail.com>
2023-03-06 18:07:06 -0600

    delete methods

--- CSC122/Week6/planet.h
+++ CSC122/Week6/planet.h
@@ -18,24 +18,6 @@ class Sphere {                                                      // first cla
 
     friend class Planet;                                            // Declare Planet class as a friend of Sphere (to access radius)
 };
-
-Sphere::Sphere() {
-    radius = 0.0;
-}
-
-void Sphere::SetRadius(double r) {
-    radius = r;
-}
-
-double Sphere::SurfaceArea() {
-    return 4.0 * M_PI * pow(radius, 2);
-}
-
-double Sphere::PlanetVolume() {
-    return (4.0/3.0) * M_PI * pow(radius, 3);
-}
-
-
 class Planet: public Sphere {                                       // Second class Planet which inherits the methods and variables of Sphere
     private: 
         double mass;                                                // Private variable mass
@@ -48,29 +30,4 @@ class Planet: public Sphere {                                       // Second cl
     
     friend ostream& operator<< (ostream&, Planet&);                 // declare the operator overload for cout << 
 };  
-
-Planet::Planet() {
-    mass = 0.0;
-}
-
-void Planet::SetMass(double m) {
-    mass = m;
-}
-
-double Planet::Density() {
-    return mass / PlanetVolume();
-}
-
-double Planet::Accelaration() {
-    const float G = 6.67 * pow(10, -11);
-    return (G * mass) / pow(radius, 2);
-}
-
-ostream& operator<< (ostream& out, Planet& ball) {                                                  // output the calculated values and setw to format the output
-    out << setw(37) << "Volume of Planet (m^3): " << ball.PlanetVolume() << endl;
-    out << setw(37) << "Surface Area of Planet (m^2): " << ball.SurfaceArea() << endl;
-    out << setw(37) << "Density of the Planet (kg/m^3): " << ball.Density() << endl;
-    out << setw(37) << "Accelaration due to Gravity (m/s^2): " << ball.Accelaration() << endl;
-    return out;
-}
 #endif
