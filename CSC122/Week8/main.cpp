edc090fad318555698ead5150a6d6690a115c72f
aziz-shoko <azizbek.shokosimov@gmail.com>
2023-03-10 17:59:58 -0600

    updated the display panel

--- CSC122/Week8/main.cpp
+++ CSC122/Week8/main.cpp
@@ -10,30 +10,36 @@ void Remove(vector<Person*>& person, string name);
 
 int main() {
     vector<Person*> record;                                     // Create a vector to store class pointers (we will be storing instances of class inside this vector)
-    int userInput = 0;
+    int userInput{0}, userInput2{0};
 
-    while (userInput != 6) {
+    while (userInput != 5) {
         // While loop to display the user option menus
-        userInput = ReadValue<int>("\nRegister user menu: \n\n\t1. Laborer\n\t2. Manager \n\t3. Owner\n\t4. Remove user \n\t5. Display\n\t6. Quit\n\nChoose number option to register: ");
+        userInput = ReadValue<int>("\nRegister user menu: \n\n\t1. Add Person\n\t2. Display List \n\t3. Sort List\n\t4. Remove User \n\t5. Quit\n\nChoose number option to register: ");
         
-        if (userInput == 1) {                                   // choice one Laborer option
-            record.push_back(new Laborer);                      // allocate Laborer class and store in vector person
-            record[record.size()-1] -> Input();                 // address the last element class inside the vector person and call function Input()
-        } else if (userInput == 2) {                            // the rest is the same thing but different options
-            record.push_back(new Manager);
-            record[record.size()-1] -> Input();
+        if (userInput == 1) {
+            userInput2 = ReadValue<int>("Choose Type of Person to Add: \n\t1. Owner \n\t2. Manager \n\t3. Laborer\n\nChoose number option to register: ");
+            if (userInput2 == 1) {                                   // choice one Laborer option
+                record.push_back(new Laborer);                      // allocate Laborer class and store in vector person
+                record[record.size()-1] -> Input();                 // address the last element class inside the vector person and call function Input()
+            } else if (userInput2 == 2) {                            // the rest is the same thing but different options
+                record.push_back(new Manager);
+                record[record.size()-1] -> Input();
+            } else if (userInput2 == 3) {
+                record.push_back(new Owner);
+                record[record.size()-1] -> Input();
+            }     
+        } else if (userInput == 2) {
+            cout << endl;
+            Display(record);
         } else if (userInput == 3) {
-            record.push_back(new Owner);
-            record[record.size()-1] -> Input();
+            cout << "N/A";
         } else if (userInput == 4) {
             string name;
             cout << "\nEnter Username to remove: ";
             cin >> name;
             Remove(record, name);
         } else if (userInput == 5) {
-            cout << endl;
-            Display(record);                                    // call function display to display the elements of stored classes inside the vector person
-        } else if (userInput == 6) {
+            cout << "\nExiting..." << endl; 
             break;
         }
     } 
