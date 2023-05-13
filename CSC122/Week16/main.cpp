01b64b167901f1c4167471ed5bf4da3c4b5b68ee
aziz-shoko <azizbek.shokosimov@gmail.com>
2023-05-12 20:57:42 -0500

    comment

--- CSC122/Week16/main.cpp
+++ CSC122/Week16/main.cpp
@@ -4,16 +4,16 @@
 #include "input.h"
 using namespace std;
 
-void DisplayUnion(set<int>&, set<int>&, set<int>&);
+void DisplayUnion(set<int>&, set<int>&, set<int>&);                             // declare functions
 void DisplayIntersection(set<int>&, set<int>&, set<int>&);
 void DisplayDifference(set<int>&, set<int>&, set<int>&);
 double Average(set<int>&);
 
 int main() {
-    set<int> set1, set2, set3;
+    set<int> set1, set2, set3;                                                  // declare sets
     int userInput;
 
-    while (userInput != 7) {
+    while (userInput != 7) {                                                    // while loop menu
         userInput = ReadValue<int> ("\n\t1. Enter Set One\n\t2. Enter Set Two\n\t3. Enter Set Three\n\t4. Display Union\n\t5. Display Intersection\n\t6. Display Difference\n\t7. Quit\n\nChoose Option: ");
 
         if (userInput == 1) {
@@ -42,7 +42,6 @@ int main() {
             }
         } else if (userInput == 4) {
             DisplayUnion(set1, set2, set3);
-
         } else if (userInput == 5) {
             DisplayIntersection(set1, set2, set3);
         } else if (userInput == 6) {
@@ -53,11 +52,11 @@ int main() {
 }
 
 void DisplayUnion(set<int>& set1, set<int>& set2, set<int>& set3) {
-    set<int> unionSet = set1;
-    unionSet.insert(set2.begin(), set2.end());
+    set<int> unionSet = set1;                                                           // make duplicate set
+    unionSet.insert(set2.begin(), set2.end());                                          // use algorithm functions to combine the sets for Union
     unionSet.insert(set3.begin(), set3.end());
 
-    cout << "Union of three sets: ";
+    cout << "Union of three sets: ";                                                    // Display the results
     for (int i : unionSet) {
         cout << i << " ";
     }
@@ -65,17 +64,20 @@ void DisplayUnion(set<int>& set1, set<int>& set2, set<int>& set3) {
 }
 
 void DisplayIntersection(set<int>& set1, set<int>& set2, set<int>& set3) {
-    set<int> intersectionSet;
+    set<int> intersectionSet;                                                           // make new intersectionSet to store the new values
+
+    // Store the common values using the set intersection function and algorithm functions
     set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(), inserter(intersectionSet, intersectionSet.begin()));
     set_intersection(intersectionSet.begin(), intersectionSet.end(), set3.begin(), set3.end(), inserter(intersectionSet, intersectionSet.begin()));
 
-    cout << "Intersection of three sets: ";
+    cout << "Intersection of three sets: ";                                             // display
     for (int i : intersectionSet) {
         cout << i << " ";
     }
     cout << "\nAverage: " << Average(intersectionSet) << endl;
 }
 
+// The same as the functions above but use difference specific set functions for this one
 void DisplayDifference(set<int>& set1, set<int>& set2, set<int>& set3) {
     set<int> differenceSet;
     set_difference(set1.begin(), set1.end(), set2.begin(), set2.end(), inserter(differenceSet, differenceSet.begin()));
@@ -88,7 +90,7 @@ void DisplayDifference(set<int>& set1, set<int>& set2, set<int>& set3) {
     cout << "\nAverage: " << Average(differenceSet) << endl;
 }
 
-double Average(set<int>& listSet) {
+double Average(set<int>& listSet) {                                                     // average calculator function
     double total = 0;
     for (int poo : listSet) {
         total += poo;
